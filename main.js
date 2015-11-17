f23d408b8ff7111f92315032885bacd3ad521a28
Ranagan <ryanflanagan12@gmail.com>
2015-11-17 20:45:26 +0000

    Get method working to return all

--- main.js
+++ main.js
@@ -24,19 +24,20 @@ db.serialize(function()
     {
         stmt.run(fill.GardaStation, fill.Y2008, fill.Y2009, fill.Y2010, fill.Y2011, fill.Y2012, fill.Y2013, fill.Crime); 
         
-        //Uncomment this to see if the database is being filled with 
+        //Uncomment this to see if the database is being filled with the data
         //console.log(fill.GardaStation, fill.Y2008, fill.Y2009, fill.Y2010, fill.Y2011, fill.Y2012, fill.Y2013, fill.Crime);
     });
     
-    db.run('CREATE TABLE educationLevel(Education Text, 15to19Years INTEGER, 20to24Years INTEGER, 25to29Years INTEGER, 30to34Years INTEGER, Y2012 INTEGER, Y2013 INTEGER, Crime Text)');
+    db.run('CREATE TABLE educationLevel(Education Text, a15to19Years INTEGER, a20to24Years INTEGER, a25to29Years INTEGER, a30to34Years INTEGER, a35to39Years INTEGER, a40to44Years INTEGER, a45to49Years INTEGER, a50to54 INTEGER, a55to59 INTEGER, a60to64Years INTEGER, a65to69Years INTEGER, a70to74Years INTEGER, a75to79Years INTEGER, a80to84Years INTEGER, a85andOver INTEGER, AllAges INTEGER)');
     
-    var stmt = db.prepare('INSERT INTO educationLevel VALUES (?,?,?,?,?,?,?,?)');
-    crimes.forEach(function (fill) 
+    var stmt = db.prepare('INSERT INTO educationLevel VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    education.forEach(function (fill) 
     {
-        stmt.run(fill.GardaStation, fill.Y2008, fill.Y2009, fill.Y2010, fill.Y2011, fill.Y2012, fill.Y2013, fill.Crime); 
+        stmt.run(fill.Education, fill.a15to19Years, fill.a20to24Years, fill.a25to29Years, fill.a30to34Years, fill.a35to39Years, fill.a40to44Years, fill.a45to49Years, fill.a50to54Years, fill.a55to59Years, fill.a60to64Years, fill.a65to69Years, fill.a70to74Years, fill.a75to79Years, fill.a80to84Years, fill.a85andOver, fill.AllAges); 
         
-        //Uncomment this to see if the database is being filled with 
-        //console.log(fill.GardaStation, fill.Y2008, fill.Y2009, fill.Y2010, fill.Y2011, fill.Y2012, fill.Y2013, fill.Crime);
+        //Uncomment this to see if the database is being filled with the data
+        //console.log(fill.Education, fill.a15to19Years, fill.a20to24Years, fill.a25to29Years, fill.a30to34Years, fill.a35to39Years, fill.a40to44Years, fill.a45to49Years, fill.a50to54Years, fill.a55to59Years, fill.a60to64Years, fill.a65to69Years, fill.a70to74Years, fill.a75to79Years, fill.a80to84Years, fill.a85andOver, fill.AllAges);
+        
     });
     
     stmt.finalize();
@@ -49,6 +50,22 @@ db.serialize(function()
 app.get('/', function(req, res) {
     res.send("This is the api working, apparently.");
     console.log("Port 8000: This is working!?");
+});
+
+// Get method to get all crimes when /allcrimes is put at end of url
+app.get('/allcrimes', function(req, res){
+  db.all("SELECT * FROM crimeRates", function(err, row) {
+    rowString = JSON.stringify(row, null, '\t');
+    res.sendStatus(rowString);
+  });
+});
+
+// Get method to get all education data when /alleducation is put at end of url
+app.get('/alleducation', function(req, res){
+  db.all("SELECT * FROM educationLevel", function(err, row) {
+    rowString = JSON.stringify(row, null, '\t');
+    res.sendStatus(rowString);
+  });
 });
 
 //Start the server
